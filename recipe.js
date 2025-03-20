1ab520e24dbeecb229cd05082b476308f1f4430b
Christine Joy Sorronda <christinejoysorronda@Christines-MacBook-Air.local>
2025-03-20 17:01:33 +0800

    first commit

---
+++ recipe.js
@@ -0,0 +1,11 @@
+const mongoose = require("./db");
+
+const recipeSchema = new mongoose.Schema({
+  name: String,
+  category: String,
+  ingredients: [String],
+  instructions: String
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe;
